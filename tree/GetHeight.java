fcff55a5822cebdb1b3075e964ac792db499fe6d
Ketan Kumar <56977429+ketangupta1@users.noreply.github.com>
2021-01-21 23:31:31 +0530

    Create GetHeight.java

---
+++ tree/GetHeight.java
@@ -0,0 +1,24 @@
+public class Solution {
+
+	/*	TreeNode structure 
+	 * 
+	 * class TreeNode<T> {
+		T data;
+		ArrayList<TreeNode<T>> children;
+
+		TreeNode(T data){
+			this.data = data;
+			children = new ArrayList<TreeNode<T>>();
+		}
+	}*/
+
+	public static int getHeight(TreeNode<Integer> root){
+		/* Your class should be named Solution
+		 * Don't write main().
+		 * Don't read input, it is passed as function argument.
+		 * Return output and don't print it.
+		 * Taking input and printing output is handled automatically.
+		 */
+	}
+
+}
