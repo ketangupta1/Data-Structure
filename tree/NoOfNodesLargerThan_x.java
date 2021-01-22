a98f55847a6f6980903dd54d04c4e36fe8ebef93
Ketan Kumar <56977429+ketangupta1@users.noreply.github.com>
2021-01-22 22:51:57 +0530

    Create NoOfNodesLargerThan_x.java

---
+++ tree/NoOfNodesLargerThan_x.java
@@ -0,0 +1,33 @@
+public class NoOfNodesLargerThan_x {
+
+/*	TreeNode class 
+ * 
+ * class TreeNode<T> {
+		T data;
+		ArrayList<TreeNode<T>> children;
+
+		TreeNode(T data){
+			this.data = data;
+			children = new ArrayList<TreeNode<T>>();
+		}
+	}*/
+
+	
+	public static int numNodeGreater(TreeNode<Integer> root,int x){
+
+		// Write your code here		
+
+        if(root==null)
+            return 0;
+        int count=0;
+        if(root.data>x)
+            count+=1;
+        for(int i=0;i<root.children.size();i++)
+        {
+            int childValue=numNodeGreater(root.children.get(i),x);
+            count+=childValue;
+        }
+        return count;
+	}
+	
+}
