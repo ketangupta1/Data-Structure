be939c8dd7d47315db87ab489886bdb5f0c7695b
Ketan Kumar <56977429+ketangupta1@users.noreply.github.com>
2021-03-29 11:06:08 +0530

    Create MinCountOfSquare.java
    Given an integer N, find and return the count of minimum numbers required to represent N as a sum of squares.
    That is, if N is 4, then we can represent it as : {1^2 + 1^2 + 1^2 + 1^2} and {2^2}. The output will be 1, as 1 is the minimum count of numbers required to represent N as sum of squares.

---
+++ DP/MinCountOfSquare.java
@@ -0,0 +1,30 @@
+// Given an integer N, find and return the count of minimum numbers required to represent N as a sum of squares.
+// That is, if N is 4, then we can represent it as : {1^2 + 1^2 + 1^2 + 1^2} and {2^2}. The output will be 1, as 1 is
+// the minimum count of numbers required to represent N as sum of squares.
+
+
+public class Solution {
+	
+	public static int minCount(int n) {
+		// Write your code here
+	if(n<=3) 
+		return n;
+        int count[] = new int[n+1];
+        count[1] = 1;
+        count[2] = 2;
+        count[3] = 3;
+
+        for(int i = 4 ; i<=n ; i++)
+        {
+            int ans = i;
+            for(int j = 1 ; j<= i/2 ; j++){
+                int k = i - (j*j);
+                if(k>=0)
+                ans = Math.min(ans , count[k] + 1 );
+            }
+            count[i] = ans;
+        }
+
+        return count[n];
+    }
+}
