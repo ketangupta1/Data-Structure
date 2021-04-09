506bfde2eb8445ab6d24816e5b3a8756d8deb1ed
Ketan Kumar <56977429+ketangupta1@users.noreply.github.com>
2021-04-09 16:19:34 +0530

    Update BfsPrint.java

--- graph/BfsPrint.java
+++ graph/BfsPrint.java
@@ -5,7 +5,7 @@ import java.io.InputStreamReader;
 import java.io.IOException;
 import java.util.*;
 
-public class Solution {
+public class BfsPrint {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
         
